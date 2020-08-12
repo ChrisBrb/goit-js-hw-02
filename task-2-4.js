432afdc3d510fbd00185b12c8fd9875be74f327d
Christina Burbulevych <christina.brb@gmail.com>
2020-08-12 22:10:20 +0300

    task-2-4

--- task-2-4.js
+++ task-2-4.js
@@ -1,26 +1,25 @@
-//Напиши функцию formatString(string) которая принимает строку и форматирует ее если необходимо.
+//Напиши функцию formatString(string, maxLength = 40) которая принимает строку и форматирует ее если необходимо.
 
-//Если длина строки не превышает 40 символов, функция возвращает ее в исходном виде.
-//Если длина больше 40 символов, то функция обрезает строку до 40-ка символов и добавляет в конец строки троеточие '...', после чего возвращает укороченную версию.
-const formatString = function (string) {
-  // твой код
-};
+//Если длина строки не превышает maxLength, функция возвращает ее в исходном виде.
+//Если длина больше maxLength, то функция обрезает строку до размера maxLength символов и добавляет в конец строки троеточие ..., после чего возвращает укороченную версию.
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(formatString("Curabitur ligula sapien, tincidunt non."));
-// вернется оригинальная строка
+function formatString (string, maxLength = 40) {
+  // Write code under this line
+  let message = '';
+  if (string.length <= maxLength) {
+    message = string;
+  } else if (string.length >= maxLength) {
+    message = string.split('');
+    message = string.slice(0, maxLength) + ('...');
+  }
+  return message
+}
 
-console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
-// вернется форматированная строка
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// 'Curabitur ligula sapien, tincidunt non.'
 
-console.log(formatString("Curabitur ligula sapien."));
-// вернется оригинальная строка
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// 'Vestibulum facilisis, purus nec pulvinar...'
 
-console.log(
-  formatString(
-    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
-  )
-);
-// вернется форматированная строка
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
+// 'Vestibulum facilisis, purus ne...'
