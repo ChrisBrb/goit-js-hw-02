7b6e5e9c51f582f7bee4b22487010be11f8996e0
Christina Burbulevych <christina.brb@gmail.com>
2020-08-20 12:12:32 +0300

    task-2-8

--- task-2-7.js
+++ task-2-7.js
@@ -2,7 +2,7 @@
 function filterArray(array) {
   'use strict';
   const numbers = [];
-  for(let i = 0; i < array.length; i += 1) {
+  for (let i = 0; i < array.length; i += 1) {
     // Write code under this line
     if (Number.isFinite (array[i])) {
     numbers.push(array[i]) }
